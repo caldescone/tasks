969c5c31c62fc33c0c89aeec6eff772fc41b5b77
Christopher Calderone <chriscalderone19@gmail.com>
2024-02-15 19:00:13 -0500

    added header, ordered list, colored the header

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,20 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
+                return <h1>A website about Peanut M&Ms</h1>
+                <img
+                    src="../images/peanutm&ms.avif"
+                    alt="A pack of peanut M&Ms"
+                />
                 UD CISC275 with React Hooks and TypeScript - Christopher
                 Calderone - Hello World
             </header>
+            List with Three Elements:
+            <ol>
+                <li> Peanuts </li>
+                <li> Chocolate </li>
+                <li> Goodness </li>
+            </ol>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
