b47cc30375e17c61dd62cc5a283c60cb811e86e6
Christopher Calderone <chriscalderone19@gmail.com>
2024-02-20 21:21:56 -0500

    fixed the makeMath function. Only injectPositives left

--- src/arrays.ts
+++ src/arrays.ts
@@ -95,7 +95,7 @@ export function allRGB(colors: string[]): boolean {
  * And the array [] would become "0=0".
  */
 export function makeMath(addends: number[]): string {
-    if (addends == null) {
+    if (addends == null || addends.length === 0) {
         return "0=0";
     }
     const sum = addends.reduce((total, currentVal) => total + currentVal, 0);
