3594d577a1d4a56669a650afc8f937dffc4a9dec
Christopher Calderone <chriscalderone19@gmail.com>
2024-02-26 22:55:57 -0500

    finished toMarkdown, about to test

--- src/objects.ts
+++ src/objects.ts
@@ -84,7 +84,13 @@ export function toShortForm(question: Question): string {
  * Check the unit tests for more examples of what this looks like!
  */
 export function toMarkdown(question: Question): string {
-    return "";
+    if (question.type === "multiple_choice_question") {
+        return `${"# " + question.name}\n${
+            question.body
+        }\n${question.options.map((option: string) => `- ${option}`)}`;
+    } else {
+        return `${"# " + question.name}\n${question.body}`;
+    }
 }
 
 /**
