58d5fb81446ca6c0357542fe739da2ba5c0403e2
Christopher Calderone <chriscalderone19@gmail.com>
2024-02-27 21:50:40 -0500

    hope this works, would be cool first try. this is sumPublishedPoints being finished and tested shortly

--- src/nested.ts
+++ src/nested.ts
@@ -82,7 +82,16 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const sumPublishedPoints = questions.reduce(
+        (currentSum: number, questions: Question) => {
+            if (questions.published) {
+                currentSum += 1;
+            }
+            return currentSum;
+        },
+        0
+    );
+    return sumPublishedPoints;
 }
 
 /***
