065f38afc0f5420e0d8e14fc3cea8f09acd65367
Christopher Calderone <chriscalderone19@gmail.com>
2024-02-27 21:40:10 -0500

    fixed the getNonEmptyQuestions function by using a different way to check

--- src/nested.ts
+++ src/nested.ts
@@ -20,11 +20,11 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
     const nonEmptyQuestions = questions.filter(
         (questions: Question): boolean =>
-            questions.body === "" &&
-            questions.expected === "" &&
-            questions.options.length === 0
+            questions.body !== "" ||
+            questions.expected !== "" ||
+            questions.options.length !== 0
     );
-    return [];
+    return nonEmptyQuestions;
 }
 
 /***
