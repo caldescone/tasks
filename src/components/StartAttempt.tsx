fc05eeb05a8dcbebd3685a16ba9732f28865bd4a
Christopher Calderone <chriscalderone19@gmail.com>
2024-03-03 14:21:51 -0500

    completed changeType and startAttempt, passing all tests

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,27 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [progress, setProgress] = useState<boolean>(false);
+    const [attempts, setAttempts] = useState<number>(4);
+    function start(): void {
+        setProgress(true);
+        setAttempts(attempts - 1);
+    }
+    return (
+        <div>
+            <Button onClick={start} disabled={progress || attempts === 0}>
+                Start Quiz
+            </Button>
+            <Button onClick={() => setProgress(false)} disabled={!progress}>
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => setAttempts(attempts + 1)}
+                disabled={progress}
+            >
+                Mulligan
+            </Button>
+            <div>Attempts: {attempts}</div>
+        </div>
+    );
 }
